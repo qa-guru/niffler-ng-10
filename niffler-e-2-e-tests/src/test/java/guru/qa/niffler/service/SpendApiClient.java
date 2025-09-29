b92c7f2a2dfe390cecabea9e806e8e5ea7e38c86
MikeVasovsky <113435819+MikeVasovsky@users.noreply.github.com>
2025-09-29 16:11:36 +0300

    Revert "hm-1"

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendApiClient.java
@@ -3,22 +3,12 @@ package guru.qa.niffler.service;
 import guru.qa.niffler.api.SpendApi;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.model.CategoryJson;
-import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import lombok.SneakyThrows;
-import org.hibernate.AssertionFailure;
-import org.junit.jupiter.api.Assertions;
-import org.springframework.data.web.PagedModel;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpendApiClient implements SpendClient{
 
@@ -31,20 +21,13 @@ public class SpendApiClient implements SpendClient{
 
   private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
+  @SneakyThrows
   @Override
   public SpendJson createSpend(SpendJson spend) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.addSpend(spend)
-              .execute();
-    } catch (IOException e){
-      throw new AssertionError(e);
-    }
-    assertEquals(200,response.code());
-    return response.body();
+    return spendApi.createSpend(spend)
+        .execute()
+        .body();
   }
-
-
 
   @Override
   public CategoryJson createCategory(CategoryJson category) {
@@ -55,83 +38,4 @@ public class SpendApiClient implements SpendClient{
   public Optional<CategoryJson> findCategoryByNameAndUsername(String categoryName, String username) {
     throw new UnsupportedOperationException("Not implemented :(");
   }
-
-  @Override
-  public SpendJson getUserById(String id, String userName) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.getSpend(id,userName)
-              .execute();
-    }catch (IOException e){
-      throw new AssertionError(e);
-    }
-    assertEquals(200, response.code());
-    return response.body();
-  }
-
-  @Override
-  public List<SpendJson> getSpends(String username, CurrencyValues filterCurrency, Date from, Date to) {
-    final Response<List<SpendJson>>spends;
-    try {
-      spends = spendApi.getSpends(username, filterCurrency, from, to)
-              .execute();
-    }catch (IOException e){
-      throw new AssertionError(e);
-    }
-    assertEquals(200,spends.code());
-    return spends.body();
-  }
-
-  @Override
-  public SpendJson editSpend(SpendJson spend) {
-    final Response<SpendJson> response;
-    try {
-      response = spendApi.editSpend(spend)
-              .execute();
-    }catch (IOException e){
-      throw new AssertionError(e);
-    }
-    assertEquals(200,response.code());
-    return response.body();
-  }
-
-  @Override
-  public Void deleteSpends(String username, List<String> ids) {
-    final Response<Void>response;
-    try {
-      response = spendApi.deleteSpends(username,ids)
-              .execute();
-    }catch (IOException e){
-      throw new AssertionError(e);
-    }
-    assertEquals(200,response.code());
-    return response.body();
-  }
-
-  @Override
-  public PagedModel<SpendJson> getspends(String username,
-                                         CurrencyValues filterCurrency,
-                                         Date from, Date to,
-                                         String searchQuery,
-                                         Integer page,
-                                         Integer size,
-                                         List<String> sort) {
-    final Response<PagedModel<SpendJson>> response;
-    try {
-      response = spendApi.getSpends(username,
-              filterCurrency,
-              from, to,
-              searchQuery,
-              page,
-              size,
-              sort)
-              .execute();
-    }catch (IOException e){
-     throw new  AssertionError(e);
-    }
-    assertEquals(200,response.code());
-    return response.body();
-  }
-
-
 }
