b31811950096975c0a5f4b0334da6a543910815a
krylov_ilya <pptestkrylov@gmail.com>
2025-09-29 20:33:08 +0300

    hm-1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/MainPage.java
@@ -1,6 +1,8 @@
 package guru.qa.niffler.page;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -8,6 +10,8 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class MainPage {
   private final SelenideElement spendingTable = $("#spendings");
+  private final SelenideElement profileBtn = $(By.xpath("//button[@class=\"MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit MuiIconButton-sizeLarge css-1obba8g\"]"));
+
 
   public MainPage checkThatPageLoaded() {
     spendingTable.should(visible);
@@ -22,5 +26,10 @@ public class MainPage {
   public MainPage checkThatTableContains(String description) {
     spendingTable.$$("tbody tr").find(text(description)).should(visible);
     return this;
+  }
+
+  public ProfilePage goToProfilePage(){
+    profileBtn.shouldBe(visible).click();
+    return new ProfilePage();
   }
 }
