9b70601c4424333191579fb0e2f82882a6f3c715
ilya krylov <pptestkrylov@gmail.com>
2025-09-30 12:47:00 +0300

    hm-1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/ProfilePage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/ProfilePage.java
@@ -8,8 +8,8 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class ProfilePage {
-    final SelenideElement showArchiveCategoriesBtn = $(By.xpath("//span[@class=\"MuiButtonBase-root MuiSwitch-switchBase MuiSwitch-colorPrimary PrivateSwitchBase-root MuiSwitch-switchBase MuiSwitch-colorPrimary css-1voo1et\"]"));
-    final SelenideElement unarchiveCategory = $(By.xpath("//button[@class=\"MuiButtonBase-root MuiIconButton-root MuiIconButton-colorInherit MuiIconButton-sizeLarge css-1obba8g\"]"));
+    final SelenideElement showArchiveCategoriesBtn = $(By.xpath(""));
+    final SelenideElement unarchiveCategory = $(By.xpath("//span[@class=\"MuiSwitch-thumb css-19gndve\"]"));
     final SelenideElement archiveCategoryBtn = $(By.xpath("//button[@aria-label=\"Archive category\"]"));
     final SelenideElement confirmeArchiveBtn = $(By.xpath("//button[contains(text(),'Archive')]"));
     final SelenideElement confirmeBtn = $(By.xpath("//button[contains(text(),'Unarchive')]"));
